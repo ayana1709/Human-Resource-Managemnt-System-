c042e922f15fe905b5684fd7e2d632449c4ab740
ayana1709 <ayanabashachali@gmail.com>
2024-06-17 10:24:53 +0300

    Admin Sidebar:   adding items

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -2,8 +2,14 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link } from "@inertiajs/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faChartLine, faCog } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 // import Sidebar from "@/Components/Sidebar";
 function Sidebar() {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
     return (
         <div className="bg-gray-800 text-white w-64 h-full p-4">
             <ul>
@@ -37,9 +43,76 @@ function Sidebar() {
                         calander
                     </li>
                 </Link>
+                <Link href={"/dashboard"}>
+                    <li className="flex items-center p-2 hover:bg-gray-700">
+                        <FontAwesomeIcon icon={faChartLine} className="mr-2" />
+                        Schedule
+                    </li>
+                </Link>
+                <Link href={"/dashboard"}>
+                    <li className="flex items-center p-2 hover:bg-gray-700">
+                        <FontAwesomeIcon icon={faChartLine} className="mr-2" />
+                        Payroll
+                    </li>
+                </Link>
+                <Link href={"dashboard"}>
+                    <li className="flex items-center p-2 hover:bg-gray-700">
+                        <FontAwesomeIcon icon={faChartLine} className="mr-2" />
+                        Report
+                    </li>
+                </Link>
+                <Link href={"/dashboard"}>
+                    <li className="flex items-center p-2 hover:bg-gray-700">
+                        <FontAwesomeIcon icon={faChartLine} className="mr-2" />
+                        Training
+                    </li>
+                </Link>
+                <Link href={"/dashboard"}>
+                    <li className="flex items-center p-2 hover:bg-gray-700">
+                        <FontAwesomeIcon icon={faChartLine} className="mr-2" />
+                        Recurtemnt
+                    </li>
+                </Link>
+
                 <li className="flex items-center p-2 hover:bg-gray-700">
                     <FontAwesomeIcon icon={faCog} className="mr-2" />
                     Settings
+                </li>
+                <li>
+                    <div className="relative">
+                        <button
+                            onClick={toggleDropdown}
+                            className="w-full text-left py-2 px-4 rounded hover:bg-gray-700 focus:outline-none"
+                        >
+                            <FontAwesomeIcon
+                                icon={faChartLine}
+                                className="mr-2"
+                            />
+                            Dropdown
+                        </button>
+                        {dropdownOpen && (
+                            <div className="absolute left-0 w-full mt-2 bg-gray-700 rounded shadow-lg">
+                                <Link
+                                    href="/link1"
+                                    className="block py-2 px-4 hover:bg-gray-600"
+                                >
+                                    Link 1
+                                </Link>
+                                <Link
+                                    href="/link2"
+                                    className="block py-2 px-4 hover:bg-gray-600"
+                                >
+                                    Link 2
+                                </Link>
+                                <Link
+                                    href="/link3"
+                                    className="block py-2 px-4 hover:bg-gray-600"
+                                >
+                                    Link 3
+                                </Link>
+                            </div>
+                        )}
+                    </div>
                 </li>
             </ul>
         </div>
