bb2de8fdb1b4b63e47b2415616bc12bb0066fe1e
ayana1709 <ayanabashachali@gmail.com>
2024-07-04 00:44:19 +0300

    adding middle ware to Employee routrs

---
+++ app/Http/Middleware/EmployeeMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class HrMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Check if the authenticated user is an HR
+        if (Auth::check() && Auth::user()->user_type === 'department_manager') {
+            return $next($request);
+        }
+
+        // If the user is not an HR, redirect to a different page or show an error
+        return redirect('/')->with('error', 'You do not have HR access.');
+    }
+}
