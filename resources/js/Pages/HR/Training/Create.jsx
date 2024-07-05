c8c339b2bbc83ad2b10ffceeaf7ae81e907dfe27
ayana1709 <ayanabashachali@gmail.com>
2024-07-05 13:29:35 +0300

    style(trainingcreate): adding sidebar and integrate with dasshboard

--- resources/js/Pages/HR/Training/Create.jsx
+++ resources/js/Pages/HR/Training/Create.jsx
@@ -3,8 +3,19 @@
 import React, { useState } from "react";
 import { useForm } from "@inertiajs/inertia-react";
 import { usePage } from "@inertiajs/react";
+import React from "react";
+import { InertiaLink } from "@inertiajs/inertia-react";
+import { usePage } from "@inertiajs/react";
+import { Inertia } from "@inertiajs/inertia";
 
-const Create = () => {
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AdminSidebar from "@/Components/Sidebar/AdminSidebar";
+import HrSidebar from "@/Components/Sidebar/HrSidebar";
+
+const Create = ({ auth }) => {
+    const { trainings } = usePage().props;
     const { users } = usePage().props;
     const { data, setData, post, processing, errors } = useForm({
         title: "",
@@ -28,74 +39,108 @@ const Create = () => {
     };
 
     return (
-        <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
-            <h1 className="text-3xl font-bold mb-6 text-center">
-                Create Training
-            </h1>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label className="block text-gray-700 font-semibold mb-2">
-                        Title
-                    </label>
-                    <input
-                        type="text"
-                        value={data.title}
-                        onChange={(e) => setData("title", e.target.value)}
-                        className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                    />
-                    {errors.title && (
-                        <div className="text-red-500 mt-1 text-sm">
-                            {errors.title}
-                        </div>
-                    )}
+        <>
+            <AuthenticatedLayout user={auth.user}>
+                <Head title="Dashboard" />
+
+                <div className="h-screen flex flex-col">
+                    {/* <Navbar /> */}
+                    <div className="flex flex-1">
+                        <HrSidebar />
+
+                        <main className="flex-1 bg-gray-100">
+                            <div className="p-4">
+                                <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
+                                    <h1 className="text-3xl font-bold mb-6 text-center">
+                                        Create Training
+                                    </h1>
+                                    <form onSubmit={handleSubmit}>
+                                        <div className="mb-4">
+                                            <label className="block text-gray-700 font-semibold mb-2">
+                                                Title
+                                            </label>
+                                            <input
+                                                type="text"
+                                                value={data.title}
+                                                onChange={(e) =>
+                                                    setData(
+                                                        "title",
+                                                        e.target.value
+                                                    )
+                                                }
+                                                className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                            />
+                                            {errors.title && (
+                                                <div className="text-red-500 mt-1 text-sm">
+                                                    {errors.title}
+                                                </div>
+                                            )}
+                                        </div>
+                                        <div className="mb-4">
+                                            <label className="block text-gray-700 font-semibold mb-2">
+                                                Description
+                                            </label>
+                                            <textarea
+                                                value={data.description}
+                                                onChange={(e) =>
+                                                    setData(
+                                                        "description",
+                                                        e.target.value
+                                                    )
+                                                }
+                                                className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                            ></textarea>
+                                            {errors.description && (
+                                                <div className="text-red-500 mt-1 text-sm">
+                                                    {errors.description}
+                                                </div>
+                                            )}
+                                        </div>
+                                        <div className="mb-4">
+                                            <label className="block text-gray-700 font-semibold mb-2">
+                                                Select Users
+                                            </label>
+                                            {users.map((user) => (
+                                                <div
+                                                    key={user.id}
+                                                    className="mb-2"
+                                                >
+                                                    <label className="inline-flex items-center">
+                                                        <input
+                                                            type="checkbox"
+                                                            value={user.id}
+                                                            onChange={
+                                                                handleCheckboxChange
+                                                            }
+                                                            className="form-checkbox"
+                                                        />
+                                                        <span className="ml-2">
+                                                            {user.name}
+                                                        </span>
+                                                    </label>
+                                                </div>
+                                            ))}
+                                            {errors.users && (
+                                                <div className="text-red-500 mt-1 text-sm">
+                                                    {errors.users}
+                                                </div>
+                                            )}
+                                        </div>
+                                        <button
+                                            type="submit"
+                                            className="btn btn-primary px-4 py-2 bg-blue-500 text-white rounded-md shadow hover:bg-blue-600 transition duration-300"
+                                            disabled={processing}
+                                        >
+                                            Create
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </main>
+                    </div>
                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 font-semibold mb-2">
-                        Description
-                    </label>
-                    <textarea
-                        value={data.description}
-                        onChange={(e) => setData("description", e.target.value)}
-                        className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                    ></textarea>
-                    {errors.description && (
-                        <div className="text-red-500 mt-1 text-sm">
-                            {errors.description}
-                        </div>
-                    )}
-                </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 font-semibold mb-2">
-                        Select Users
-                    </label>
-                    {users.map((user) => (
-                        <div key={user.id} className="mb-2">
-                            <label className="inline-flex items-center">
-                                <input
-                                    type="checkbox"
-                                    value={user.id}
-                                    onChange={handleCheckboxChange}
-                                    className="form-checkbox"
-                                />
-                                <span className="ml-2">{user.name}</span>
-                            </label>
-                        </div>
-                    ))}
-                    {errors.users && (
-                        <div className="text-red-500 mt-1 text-sm">
-                            {errors.users}
-                        </div>
-                    )}
-                </div>
-                <button
-                    type="submit"
-                    className="btn btn-primary px-4 py-2 bg-blue-500 text-white rounded-md shadow hover:bg-blue-600 transition duration-300"
-                    disabled={processing}
-                >
-                    Create
-                </button>
-            </form>
-        </div>
+            </AuthenticatedLayout>
+        </>
     );
 };
 
--- resources/js/Pages/HR/Training/Index.jsx
+++ resources/js/Pages/HR/Training/Index.jsx
@@ -26,6 +26,9 @@ const Index = ({ auth }) => {
 
                         <main className="flex-1 bg-gray-100">
                             <div className="p-4">
+
+
+
                                 <div className="container mx-auto p-4">
                                     <h1 className="text-3xl font-bold mb-6 text-center">
                                         Trainings
@@ -95,6 +98,9 @@ const Index = ({ auth }) => {
                                         ))}
                                     </div>
                                 </div>
+
+
+                                
                             </div>
                         </main>
                     </div>
